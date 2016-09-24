9178108b570afe02b30083887ebb6eac56e53b49
chenglei_hos <chengzi12130@gmail.com>
2016-09-25 00:13:45 +0800

    jsonp

--- jstest/js2.js
+++ jstest/js2.js
@@ -4,8 +4,8 @@
     
 //if(confirm("open new window") === true)
 {
-    window.open("http://www.immoc.com/");
-    window.open()
+    // window.open("http://www.immoc.com/");
+    // window.open()
     // var url = prompt("input url to open","http://www.immoc.com/");
 
     // if(url != null)
@@ -13,4 +13,49 @@
     //     window.open(url);
     //     //document.wri
     // }
+
+    // var settings = {
+    //   "async": true,
+    //   //"crossDomain": true,
+    //   //"Access-Control-Allow-Origin":"*",
+    //   "url": "http://hisyongcheng.bakingerp.com:8102/api/ADVTEMP2/GetWhereADVTEMP2?whereClause=sInHospitalId%3D3162&orderBy=sInHospitalId",
+    //   "method": "GET",
+    //   "dataType":"JSONP",
+    //   "JSONP":"callback",
+    //   "jsonpCallback":"xxxx",
+    //   "headers": {}
+    // }
+    // document.write("hello world");
+
+    // $.ajax(settings).done(function (data) {
+    //   //console.table(response);
+    //   alert(data);
+    //   document.write("hello world");
+    //   document.write(data);
+    // });
+
+    // $.ajax(
+    //     {
+    //          "async": true,
+    //         //"crossDomain": true,
+    //         //"Access-Control-Allow-Origin":"*",
+    //         "url": "http://hisyongcheng.bakingerp.com:8102/api/ADVTEMP2/GetWhereADVTEMP2?whereClause=sInHospitalId%3D3162&orderBy=sInHospitalId",
+    //         "method": "GET",
+    //         "dataType":"JSONP",
+    //         "JSONP":"callback",
+    //         "jsonpCallback":"xxxx",
+    //         "headers": {},
+            
+    //         success:function(data){
+    //             docum.write(data);
+    //         }
+            
+    //     }
+    // );
+
+    
+
 }
+
+
+
